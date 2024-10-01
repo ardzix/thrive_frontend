a919ce0897505e47e783f79a76da2a3e286f232d
pep-sanz <pepepsr4@gmail.com>
2024-10-01 11:49:35 +0700

    added default password and email

--- src/pages/auth/LoginContainer.tsx
+++ src/pages/auth/LoginContainer.tsx
@@ -62,6 +62,11 @@ export default function LoginContainer() {
         </div>
         <div className="p-6 space-y-5">
           <h2 className="text-2xl font-bold">Login</h2>
+          <div className="">
+            email: admin
+            <br />
+            password: admin
+          </div>
           <FormGenerator
             hookForm={hookFormGenerator}
             onFinish={handleSubmuit}
